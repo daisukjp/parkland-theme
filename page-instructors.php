4ff2f42f8ebc899de90d11a1666489ffbf51e26d
ebriscall1 <evanbriscall@gmail.com>
2023-03-31 17:20:58 -0600

    trying to loop custom posts

--- page-instructors.php
+++ page-instructors.php
@@ -17,79 +17,83 @@
 <main>
     <section>
 
-    <div class="instructors-page-container">
+        <div class="instructors-page-container">
 
-        <div class="instructors-banner">
-            <h2>Instructors</h2>
+            <div class="instructors-banner">
+                <h2>Instructors</h2>
 
-            <div class="instructors-rectangle">
-            </div>
-        </div>
-
-    </div>
-
-    <div class="instructors-section">
-        <h2>Meet Our Instructors <img src="/wp-content/themes/parkland-theme/images/brushstroke.svg" alt="styled brush stroke"></h2>
-
-
-        <div class="instructor-container-1">
-            <div class="instructor-image">
-                <img src="/wp-content/themes/parkland-theme/images/ptc-john.webp" alt="picture of John" width="360" height="240">
-            </div>
-
-            <div class="instructor-information">
-                <h3>John</h3>
-                <h4>Instructor Information</h4>
-                <p>Hi, I am John Tummers,</p>
-                <p>I have been studying and teaching Tai Chi and Chi Kung for over 40 years.</p>
-                <p>In 1991, I founded the Parkland Tai Chi Association as a 100% non-profit organization and with many other volunteers continue to pass on the techniques and practices. It gives me utmost pleasure to share this with others.</p>
+                <div class="instructors-rectangle">
+                </div>
             </div>
 
         </div>
-        <div class="instructor-container-2">
-            <div class="instructor-image">
-                <img src="/wp-content/themes/parkland-theme/images/ptc-grace.webp" alt="picture of John" width="360" height="240">
+
+        <div class="instructors-section">
+            <?php while ( have_posts()) : the_post(); ?>
+            <?php $instructor_name = get_field('instructor_name'); ?>
+            <?php $image =  wp_get_attachment_image_src(get_sub_field('instructor_image')); ?>
+            <?php $description = get_field('description'); ?>
+
+            <h2>Meet Our Instructors <img src="/wp-content/themes/parkland-theme/images/brushstroke.svg" alt="styled brush stroke"></h2>
+
+
+            <div class="instructor">
+
+                <div class="instructor-image">
+                <img src="<?php echo $image[0]; ?>"/>
+                </div>
+
+                <div class="instructor-information">
+                    <h3><?php echo $instructor_name;?></h3>
+                    <h4>Instructor Information</h4>
+                    <p><?php echo $description;?></p>
+                </div>
+
+            </div>
+            <div class="instructor-container-2">
+                <div class="instructor-image">
+                    <img src="/wp-content/themes/parkland-theme/images/ptc-grace.webp" alt="picture of John" width="360" height="240">
+                </div>
+
+                <div class="instructor-information">
+                    <h3>Grace</h3>
+                    <h4>Instructor Information</h4>
+                    <p>Hi, I am Grace,</p>
+                    <p>For years I admired the slow, calm, rhythmic movements of Tai Chi. I joined Parkland Tai Chi in 2000, and from the very first couple of moves I was hooked.</p>
+                    <p>Tai Chi is definitely for me, and I am very passionate about it. I enjoy teaching both beginner and intermediate classes immensely and I hope that my passion for the practice is reflected in my teaching.</p>
+                </div>
             </div>
 
-            <div class="instructor-information">
-                <h3>Grace</h3>
-                <h4>Instructor Information</h4>
-                <p>Hi, I am Grace,</p>
-                <p>For years I admired the slow, calm, rhythmic movements of Tai Chi. I joined Parkland Tai Chi in 2000, and from the very first couple of moves I was hooked.</p>
-                <p>Tai Chi is definitely for me, and I am very passionate about it. I enjoy teaching both beginner and intermediate classes immensely and I hope that my passion for the practice is reflected in my teaching.</p>
+            <div class="instructor-container-3">
+                <div class="instructor-image">
+                    <img src="/wp-content/themes/parkland-theme/images/ptc-charlene.webp" alt="picture of John" width="360" height="240">
+                </div>
+
+                <div class="instructor-information">
+                    <h3>Charlene</h3>
+                    <h4>Instructor Information</h4>
+                    <p>Hi, I am Charlene,</p>
+                    <p>I started my Tai Chi journey in 2018. I needed to do some exercise and thought Tai Chi would be good as it is low impact. I soon got hooked on learning about how Tai Chi can greatly improve all areas of my life. I will continue to learn and grow..</p>
+                </div>
+            </div>
+
+            <div class="instructor-container-4">
+                <div class="instructor-image">
+                    <img src="/wp-content/themes/parkland-theme/images/ptc-alex.webp" alt="picture of John" width="360" height="240">
+                </div>
+
+                <div class="instructor-information">
+                    <h3>Alex</h3>
+                    <h4>Instructor Information</h4>
+                    <p>Hi, I am Alex</p>
+                    <p>I have been studying and teaching Tai Chi and Chi Kung for over 40 years.</p>
+                    <p>In 1991, I founded the Parkland Tai Chi Association as a 100% non-profit organization and with many other volunteers continue to pass on the techniques and practices. It gives me utmost pleasure to share this with others.</p>
+                </div>
             </div>
         </div>
-
-        <div class="instructor-container-3">
-            <div class="instructor-image">
-                <img src="/wp-content/themes/parkland-theme/images/ptc-charlene.webp" alt="picture of John" width="360" height="240">
-            </div>
-
-            <div class="instructor-information">
-                <h3>Charlene</h3>
-                <h4>Instructor Information</h4>
-                <p>Hi, I am Charlene,</p>
-                <p>I started my Tai Chi journey in 2018. I needed to do some exercise and thought Tai Chi would be good as it is low impact. I soon got hooked on learning about how Tai Chi can greatly improve all areas of my life. I will continue to learn and grow..</p>
-            </div>
-        </div>
-
-        <div class="instructor-container-4">
-            <div class="instructor-image">
-                <img src="/wp-content/themes/parkland-theme/images/ptc-alex.webp" alt="picture of John" width="360" height="240">
-            </div>
-
-            <div class="instructor-information">
-                <h3>Alex</h3>
-                <h4>Instructor Information</h4>
-                <p>Hi, I am Alex</p>
-                <p>I have been studying and teaching Tai Chi and Chi Kung for over 40 years.</p>
-                <p>In 1991, I founded the Parkland Tai Chi Association as a 100% non-profit organization and with many other volunteers continue to pass on the techniques and practices. It gives me utmost pleasure to share this with others.</p>
-            </div>
-        </div>
-    </div>
     </section>
 </main>
 
-</div>
 
+<?php endwhile; ?>
 <?php get_footer(); ?>
