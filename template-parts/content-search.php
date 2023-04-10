deb47f9764b63202cf429c3e6a9e2bb376ed8da3
Daisuke Sato <dsato2@nait.ca>
2023-04-10 00:52:40 -0600

    search page completed

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,6 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="search-page-container">
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
@@ -25,11 +26,26 @@
 
 	<?php parkland_theme_post_thumbnail(); ?>
 
+	<div>
+		<?php
+        $image = get_field('blog_image');
+		?>
+
+		<?php if( !empty( $image ) ): ?>
+			<a href="<?php the_permalink(); ?> ">
+				<img src="<?php echo esc_url($image['sizes']['large']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="blog-img">
+			</a>
+		<?php endif ?>
+	</div>
+
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
+	
+
 	<footer class="entry-footer">
 		<?php parkland_theme_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
+	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
